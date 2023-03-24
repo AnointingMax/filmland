53e9cdd5f18a36dbb982805fb514c0990aa806d6
Anointing Maxwell <anointingmax@gmail.com>
2023-03-24 12:28:36 +0100

    Finished design of top nav

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -1,8 +1,34 @@
 import React from "react";
 import styles from "./topnav.module.css";
+import Logo from "assets/logo.png";
+import { BurgerIcon, SearchIcon } from "assets/svg";
 
 const TopNav = () => {
-	return <div>TopNav</div>;
+	return (
+		<header>
+			<img src={Logo} alt="FilmLand" className={styles.logo} />
+			<Search />
+			<div className={styles.left}>
+				<p>Hi, $Fullname</p>
+				<a href="/#" className={styles.burger}>
+					<BurgerIcon />
+				</a>
+			</div>
+		</header>
+	);
+};
+
+const Search = () => {
+	return (
+		<div className={styles.searchWrapper}>
+			<input
+				type="text"
+				placeholder="What do you want to watch?"
+				className={styles.search}
+			/>
+			<SearchIcon />
+		</div>
+	);
 };
 
 export default TopNav;
