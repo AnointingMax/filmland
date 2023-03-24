4d5926b4a946a980c4e1d10c33330b91503508d8
Anointing Maxwell <anointingmax@gmail.com>
2023-03-24 10:46:01 +0100

    Moved svgs to proper folder

---
+++ src/assets/svg/index.js
@@ -0,0 +1,27 @@
+import { ReactComponent as BurgerIcon } from "./burger.svg";
+import { ReactComponent as FacebookIcon } from "./facebook.svg";
+import { ReactComponent as HeartIcon } from "./heart.svg";
+import { ReactComponent as IMDBIcon } from "./imdb.svg";
+import { ReactComponent as InstagramIcon } from "./instagram.svg";
+import { ReactComponent as LeftArrowIcon } from "./left-arrow.svg";
+import { ReactComponent as PlayIcon } from "./play.svg";
+import { ReactComponent as RightArrowIcon } from "./right-arrow.svg";
+import { ReactComponent as SearchIcon } from "./search.svg";
+import { ReactComponent as TomatoIcon } from "./tomato.svg";
+import { ReactComponent as TwitterIcon } from "./twitter.svg";
+import { ReactComponent as YoutubeIcon } from "./youtube.svg";
+
+export {
+	BurgerIcon,
+	FacebookIcon,
+	HeartIcon,
+	IMDBIcon,
+	InstagramIcon,
+	LeftArrowIcon,
+	PlayIcon,
+	RightArrowIcon,
+	SearchIcon,
+	TomatoIcon,
+	TwitterIcon,
+	YoutubeIcon,
+};
