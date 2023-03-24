17b5162ba8826eb145920b70aa6bce81315e3848
Anointing Maxwell <anointingmax@gmail.com>
2023-03-24 11:27:55 +0100

    Finished design of Footer

--- src/components/BottomNav/index.js
+++ src/components/BottomNav/index.js
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+	FacebookIcon,
+	InstagramIcon,
+	TwitterIcon,
+	YoutubeIcon,
+} from "assets/svg";
+import styles from "./bottomnav.module.css";
+import { Link } from "react-router-dom";
 
 const BottomNav = () => {
-	return <div>BottomNav</div>;
+	return (
+		<footer>
+			<div className={styles.socialIcons}>
+				<Link to="#">
+					<FacebookIcon />
+				</Link>
+				<Link to="#">
+					<InstagramIcon />
+				</Link>
+				<Link to="#">
+					<TwitterIcon />
+				</Link>
+				<Link to="#">
+					<YoutubeIcon />
+				</Link>
+			</div>
+			<div className={styles.footerLinks}>
+				<Link to={"#"} className={styles.footerLink}>
+					Conditions of Use
+				</Link>
+				<Link to={"#"} className={styles.footerLink}>
+					Privacy & Policy
+				</Link>
+				<Link to={"#"} className={styles.footerLink}>
+					Press Room
+				</Link>
+			</div>
+			<p className={styles.copyright}>© 2021 Movix</p>
+		</footer>
+	);
 };
 
 export default BottomNav;
